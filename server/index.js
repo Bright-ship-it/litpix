a38cf8cef358e7a8f76f3930a03a839810a1feeb
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-08-13 03:44:33 +0300

    Redux setup complete

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Luminzu:Luminzu254@cluster0.lpzt8lw.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
